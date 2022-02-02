35fa344de085bb861d61a1015824e858518d13af
PiotrWesoly <Piotrpwesoly@gmail.com>
2022-02-02 17:12:32 +0100

    Bluetooth: add bluetooth activity (to be continued...)

--- app/src/main/java/com/example/thesis/MainActivity.java
+++ app/src/main/java/com/example/thesis/MainActivity.java
@@ -5,10 +5,12 @@ import androidx.cardview.widget.CardView;
 
 import android.content.Context;
 import android.content.Intent;
+import android.media.Image;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import static android.content.ContentValues.TAG;
@@ -23,18 +25,40 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         setContentView(R.layout.activity_main);
 
         CardView LivingRoomL = (CardView) findViewById(R.id.LivingRoom);
+        CardView Lock = (CardView) findViewById(R.id.lockdoor);
+        CardView bluetooth = (CardView) findViewById(R.id.bluetooth);
+
         LivingRoomL.setOnClickListener(this);
+        Lock.setOnClickListener(this);
+        bluetooth.setOnClickListener(this);
 
     }
 
     @Override
     public void onClick(View v) {
+        ImageView lock = (ImageView) findViewById(R.id.lock);
+        TextView lock_text = (TextView) findViewById(R.id.lock_text);
+        Context context = v.getContext();
         Log.d(TAG, "WESZLO");
 
         switch (v.getId()) {
             case R.id.LivingRoom:
-                Context context = v.getContext();
                 context.startActivity(new Intent(context,LivingRoom.class));
+                break;
+            case R.id.lockdoor:
+                if(lock_text.getText() == "LOCKED")
+                {
+                    lock.setImageResource(R.drawable.lock_icon_unlocked);
+                    lock_text.setText("UNLOCKED");
+                }
+                else
+                {
+                    lock.setImageResource(R.drawable.lock_icon_locked);
+                    lock_text.setText("LOCKED");
+                }
+                break;
+            case R.id.bluetooth:
+                context.startActivity(new Intent(context,Bluetooth.class));
                 break;
             default:
                 break;
